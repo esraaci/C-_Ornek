3449bf200dc6b83c508f90db1d8dd6f7162b5ef8
esraaci <esraaci25@gmail.com>
2024-10-11 18:13:42 +0300

    C#_Ornek
    C#_Ornek

---
+++ Ders1/Ders1/Program.cs
@@ -0,0 +1,14 @@
+﻿int sayi1, sayi2, toplam = 0;
+
+Console.Write("1.Sayıyı Giriniz: ");
+sayi1 = int.Parse(Console.ReadLine());
+
+
+Console.Write("2.Sayıyı Giriniz: ");
+sayi2 = int.Parse(Console.ReadLine());
+
+toplam = sayi1 + sayi2;
+
+Console.WriteLine("Toplam: " + toplam);
+
+Console.ReadLine();
